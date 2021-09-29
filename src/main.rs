08bb419c409384181c5733b6c5071d3814d2dbc4
saresend <saresend@usc.edu>
2021-09-28 22:17:00 -0700

    adding petgraph & logos deps

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use clap::{App, Arg, SubCommand};
+
+use petgraph::graph::Graph;
+
 use std::io::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = App::new("Dotfile Parser")
         .version("1.0")
-        .author("Samuel Resendez")
-        .arg(
+        .author("Samuel Resendez") .arg(
             Arg::with_name("INPUT_FILE")
                 .short("f")
                 .takes_value(true)
@@ -17,8 +19,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut file = std::fs::File::open(filename)?;
 
     let mut dotfile_text = String::new();
-
     file.read_to_string(&mut dotfile_text);
-
+    let petgraph = parse_to_graph(dotfile_text);
     Ok(())
+}
+
+
+fn parse_to_graph(dotfile: String) -> Graph<String, String> {
+    todo!()
 }
