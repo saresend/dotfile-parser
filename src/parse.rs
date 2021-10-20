433ac01398d4de22947d60448fd60b5e31221b13
saresend <saresend@usc.edu>
2021-10-19 20:54:03 -0700

    sets up reading from Bufreader into string and then deriving the token stream from it

--- src/parse.rs
+++ src/parse.rs
@@ -2,6 +2,9 @@ use petgraph::data::Build;
 use std::fs::File;
 use std::io::BufReader;
 use std::marker::PhantomData;
+use crate::lexer::Token;
+
+use std::io::Read;
 
 pub struct DotParser<R, B>
 where
@@ -24,6 +27,14 @@ where
     }
 
     pub fn parse_into_graph(&self) -> B {
+        let token_stream = Token::lexer(self.get_token_string());
         B::default()
     }
+
+    fn get_token_string(&self) -> String {
+        let mut s = String::new();
+        self.input.read_to_string(&mut s); 
+        s
+    }
+
 }
