2e0a6c003bec9e8e99c485260bb29c013f40aa43
saresend <saresend@usc.edu>
2021-10-19 20:37:42 -0700

    partial inclusion of all AST node types

--- src/ast_nodes.rs
+++ src/ast_nodes.rs
@@ -12,16 +12,60 @@ pub enum StatementNode {
     SubgraphStatement(SubgraphNode),
 }
 
-#[derive(Clone, Debug)]
-pub struct NodeStatementNode {}
+// TODO: Should add compass pt support 
+type Port = String;
+
+type Ident = String; // using strings as Identifiers for now
 
 #[derive(Clone, Debug)]
-pub struct EdgeStatementNode {}
+pub enum EdgeOP {
+    Directed,
+    Undirected,
+}
 #[derive(Clone, Debug)]
+pub struct IDNode {
+    id: Ident,  
+}
 
+#[derive(Clone, Debug)]
+pub struct NodeStatementNode {
+    id: NodeID,
+    attributes: Vec<AttributeListNode>,
+}
+
+#[derive(Clone, Debug)]
+pub struct NodeID {
+    id: Ident,
+    port: Option<Port>,
+}
+
+#[derive(Clone, Debug)]
+pub enum EdgeRHSNode {
+   Node((EdgeOP, NodeID, Box<Option<EdgeRHSNode>>)),
+
+}
+
+#[derive(Clone, Debug)]
+pub enum EdgeStatementNode {
+    Node((NodeID, EdgeRHSNode, Vec<AttributeListNode>)),
+}
+
+#[derive(Clone, Debug)]
+pub struct AttributeNode {
+    lhs_id: IDNode,
+    rhs_id: IDNode,
+}
+    
+#[derive(Clone, Debug)]
+pub struct AttributeListNode {
+    attributes: Vec<AttributeNode>,
+}
+
+#[derive(Clone, Debug)]
 pub struct AttributeStatementNode {}
-#[derive(Clone, Debug)]
 
+
+#[derive(Clone, Debug)]
 pub struct AssignmentStatementNode {}
 
 #[derive(Clone, Debug)]
