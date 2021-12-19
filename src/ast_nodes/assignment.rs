b8085602d75188e3e251b0ff7af679a2c57c512e
saresend <saresend@usc.edu>
2021-12-18 21:02:52 -0800

    now supports vector parsing of assignment statements

--- src/ast_nodes/assignment.rs
+++ src/ast_nodes/assignment.rs
@@ -5,17 +5,16 @@ use crate::lex::{Token, PeekableLexer, Peekable};
 
 /// This is the primary node capable of parsing 
 /// constructs of the form 'ID' = 'ID'
+#[derive(Debug, PartialEq)]
 pub struct Assignment {
     lhs: ID,
     rhs: ID,
 }
 
-
-
-impl TryFrom<PeekableLexer<'_>> for Assignment {
+impl TryFrom<&mut PeekableLexer<'_>> for Assignment {
     type Error = anyhow::Error;
 
-    fn try_from(mut lexer: PeekableLexer<'_>) -> Result<Self, Self::Error> {
+    fn try_from(mut lexer: &mut PeekableLexer<'_>) -> Result<Self, Self::Error> {
         if let Some(Token::ID) = lexer.next() {
             let lhs = String::from(lexer.slice());
             if let Some(Token::Equals) = lexer.next() {
@@ -30,6 +29,30 @@ impl TryFrom<PeekableLexer<'_>> for Assignment {
 }
 
 
+impl TryFrom<&mut PeekableLexer<'_>> for Vec<Assignment> {
+    type Error = anyhow::Error;
+
+    fn try_from(token_stream: &mut PeekableLexer<'_>) -> Result<Self, Self::Error> {
+        let mut c1 = token_stream.clone();
+        let mut result = vec![];
+        while let Ok(assignment) = Assignment::try_from(&mut c1) {
+            result.push(assignment);
+            match c1.peek() {
+                Some(Token::Comma) | Some(Token::SemiColon) => {
+                    c1.next();
+                },
+                _ => {
+                    *token_stream = c1;
+                    return Ok(result);
+                },
+            }
+        }
+        *token_stream = c1;
+        Ok(result)
+    }
+
+}
+
 #[cfg(test)]
 mod tests {
     use super::Assignment;
@@ -39,8 +62,8 @@ mod tests {
     #[test]
     fn assignment_sanity_test() {
         let test_str = "color = red";
-        let plexer = PeekableLexer::from(test_str); 
-        let assignment = Assignment::try_from(plexer).unwrap();
+        let mut plexer = PeekableLexer::from(test_str); 
+        let assignment = Assignment::try_from(&mut plexer).unwrap();
         assert_eq!(assignment.lhs, String::from("color"));
         assert_eq!(assignment.rhs, String::from("red"));
     }
@@ -48,9 +71,22 @@ mod tests {
     #[test]
     fn assignment_correct_rejection_test() {
         let test_str = "color = {";
-        let plexer = PeekableLexer::from(test_str);
-        let assignment = Assignment::try_from(plexer);
+        let mut plexer = PeekableLexer::from(test_str);
+        let assignment = Assignment::try_from(&mut plexer);
         assert!(assignment.is_err());
+
+    }
+
+    #[test]
+    fn assignment_vector_comma_sanity_test() {
+        let test_str = "color = red, width = hello";
+        let mut plexer = PeekableLexer::from(test_str);
+        let results = Vec::try_from(&mut plexer).unwrap();
+        assert_eq!(results[0].lhs, String::from("color"));
+        assert_eq!(results[0].rhs, String::from("red"));
+        assert_eq!(results[1], Assignment { lhs: String::from("width"), rhs: String::from("hello") });
+
+
 
     }
 }
