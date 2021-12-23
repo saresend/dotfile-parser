fa0dcc2f43a67a7523685a0f8383c9b4773627a9
saresend <saresend@usc.edu>
2021-12-23 02:38:55 +0000

    removing edge implementation since it doesn't quite work

--- src/ast_nodes/edge.rs
+++ src/ast_nodes/edge.rs
@@ -11,7 +11,6 @@ use crate::parse::{Constructable, ParseOR};
 struct Directed;
 struct Undirected;
 
-
 pub struct Edge<LHS, T> {
     lhs: LHS,
     ty: PhantomData<T>,
@@ -19,37 +18,18 @@ pub struct Edge<LHS, T> {
 
 }
 
-impl Constructable for Edge<Node, Directed> {
-    type Output = Self;
+impl Constructable for Edge<ID, Directed> {
+    type Output = Edge::<Node, Directed>;
 
     fn from_lexer(
         mut token_stream: crate::lex::PeekableLexer,
-    ) -> anyhow::Result<(Self, crate::lex::PeekableLexer), anyhow::Error> {
-        if let Some(Token::ID) = token_stream.next() {
-            let node_id = token_stream.slice().to_string();
-            if let Some(Token::DirectedEdge) = token_stream.next() {
-                let result = ParseOR::<Edge<Node, Directed>, Edge<Subgraph, Directed>>::from_lexer(
-                    token_stream.clone(),
-                )?;
-                if let Some(val) = result.0.t_val {
-                    // We'de like to return an Edge<Node, Directed>
-                    todo!()
-                } else if let Some(val) = result.0.v_val {
-                    // We'd like to return an Edge<Subgraph, Directed>
-                    todo!()
-                }
-                todo!()
-            } else {
-                Err(anyhow::anyhow!("Invalid token; expected directed edge"))
-            }
-        } else {
-            Err(anyhow::anyhow!("Invalid token; expected ID"))
-        }
+    ) -> anyhow::Result<(Self::Output, crate::lex::PeekableLexer), anyhow::Error> {
+        todo!()
     }
 }
 
 impl Constructable for Edge<Subgraph, Directed> {
-    type Output = Self;
+    type Output = Edge<Subgraph, Directed>;
     
     fn from_lexer(
         token_stream: crate::lex::PeekableLexer,
