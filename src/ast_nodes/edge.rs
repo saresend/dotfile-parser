7b2464157cb919a36fce107dd9b462657dc06c9e
saresend <saresend@usc.edu>
2021-12-24 00:32:44 +0000

    now successfully can parse really basic edge types

--- src/ast_nodes/edge.rs
+++ src/ast_nodes/edge.rs
@@ -12,13 +12,19 @@ struct Directed;
 struct Undirected;
 
 enum EdgeLHS {
-    Node(Node),
+    Node(ID),
+    Subgraph(Subgraph),
+}
+
+enum EdgeRHS<T> {
+    Edge(Edge<T>),
+    Node(ID),
     Subgraph(Subgraph),
 }
 
 pub struct Edge<T> {
     lhs: EdgeLHS,
-    rhs: Box<Edge<T>>,
+    rhs: Box<EdgeRHS<T>>,
     ty: PhantomData<T>,
     attr_list: AttributeList,
 }
@@ -29,40 +35,81 @@ impl Constructable for Edge<Directed> {
     fn from_lexer(
         token_stream: crate::lex::PeekableLexer,
     ) -> anyhow::Result<(Self::Output, crate::lex::PeekableLexer), anyhow::Error> {
-        let mut p1 = ParseOR::<Node, Subgraph>::from_lexer(token_stream.clone())?;
+        let mut p1 = ParseOR::<ID, Subgraph>::from_lexer(token_stream.clone())?;
         if let Some(lhs) = p1.0.t_val {
             if let Some(Token::DirectedEdge) = p1.1.next() {
-                let rhs = Edge::<Directed>::from_lexer(p1.1)?;
-                let cloned_attr = rhs.0.attr_list.clone();
-                Ok((
-                    Self {
-                        lhs: EdgeLHS::Node(lhs),
-                        rhs: Box::new(rhs.0),
-                        ty: PhantomData,
-                        attr_list: cloned_attr,
-                    },
-                    rhs.1,
-                ))
+                let rhs = ParseOR::<Edge<Directed>, ParseOR<ID, Subgraph>>::from_lexer(p1.1)?;
+                if let Some(v) = rhs.0.t_val {
+                    Ok((
+                        Self {
+                            lhs: EdgeLHS::Node(lhs),
+                            rhs: Box::new(EdgeRHS::Edge(v)),
+                            ty: PhantomData,
+                            attr_list: vec![],
+                        },
+                        rhs.1,
+                    ))
+                } else if let Some(inner) = rhs.0.v_val {
+                    if let Some(id) = inner.t_val {
+                    Ok((
+                        Self {
+                            lhs: EdgeLHS::Node(lhs),
+                            rhs: Box::new(EdgeRHS::Node(id)),
+                            ty: PhantomData,
+                            attr_list: vec![],
+                        },
+                        rhs.1,
+                    ))
+
+                    } else if let Some(subgraph) = inner.v_val {
+                    Ok((
+                        Self {
+                            lhs: EdgeLHS::Node(lhs),
+                            rhs: Box::new(EdgeRHS::Subgraph(subgraph)),
+                            ty: PhantomData,
+                            attr_list: vec![],
+                        },
+                        rhs.1,
+                    ))
+                    } else {
+                        Err(anyhow::anyhow!("Invalid token following node id for edge"))
+                    }
+                } else {
+                    Err(anyhow::anyhow!("Invalid token following node id for edge"))
+                }
             } else {
                 Err(anyhow::anyhow!("Invalid token following node id for edge"))
             }
         } else if let Some(lhs) = p1.0.v_val {
-            let rhs = Edge::<Directed>::from_lexer(p1.1)?;
-            let cloned_attr = rhs.0.attr_list.clone();
-
-            Ok((
-                Self {
-                    lhs: EdgeLHS::Subgraph(lhs),
-                    rhs: Box::new(rhs.0),
-                    ty: PhantomData,
-                    attr_list: cloned_attr,
-                },
-                rhs.1,
-            ))
+            todo!()
         } else {
             Err(anyhow::anyhow!(
                 "Couldn't parse either Node or Subgraph for Edge Node"
             ))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ast_nodes::{edge::Directed, Edge};
+    use crate::lex::PeekableLexer;
+    use crate::parse::Constructable;
+    use super::{EdgeLHS, EdgeRHS};
+
+    #[test]
+    fn edge_directed_statement_sanity_node_test1() {
+        let test_str = "A -> B";
+        let pb = PeekableLexer::from(test_str);
+        let res = Edge::<Directed>::from_lexer(pb).unwrap();
+        let rhs_v = *res.0.rhs;
+        let edg_lhs = res.0.lhs;
+        if let EdgeRHS::<Directed>::Node(id) = rhs_v {
+            assert_eq!("B", id);
+        } else { unreachable!() };
+
+        if let EdgeLHS::Node(id) = edg_lhs{ 
+            assert_eq!("A", id);
+        } else { unreachable!() };
     }
 }
--- src/ast_nodes/mod.rs
+++ src/ast_nodes/mod.rs
@@ -11,3 +11,19 @@ pub use statement::Statement;
 pub use subgraph::Subgraph;
 
 type ID = String;
+
+use crate::lex::{Peekable, PeekableLexer, Token};
+use crate::parse::Constructable;
+
+impl Constructable for ID {
+    type Output = Self;
+    fn from_lexer(
+        mut token_stream: crate::lex::PeekableLexer,
+    ) -> anyhow::Result<(Self::Output, crate::lex::PeekableLexer), anyhow::Error> {
+        if let Some(Token::ID) = token_stream.next() {
+            Ok((token_stream.slice().to_owned(), token_stream))
+        } else {
+            Err(anyhow::anyhow!("Expected type ID"))
+        }
+    }
+}
